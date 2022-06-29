0619e2e094f2ba29b737a954907a02482bea7efb
David M.T <thigedavidmaitho@gmail.com>
2022-06-30 02:19:43 +0300

    allow any host

--- login/settings.py
+++ login/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=t2%g)-1*y=cl#%pln3&qn*ym50bg9*gy_2(f@%y7q4u%m3v_f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
