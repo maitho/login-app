653c46ce2d9c917aadf17ea438895df238626669
David M.T <thigedavidmaitho@gmail.com>
2022-07-01 23:55:45 +0300

    update paths

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path("register", views.register, name="register"),
-    path("login_user", views.login_user, name="login_user"),
-    path("logout_user", views.logout_user, name="logout_user"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
     path("home", views.home, name="home")
 ]
